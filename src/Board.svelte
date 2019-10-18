3bf01401e27c1c35fb748f4c4232c48e7e1741b2
Benjamin Mock <mail@benjaminmock.de>
2019-10-19 00:01:52 +0200

    Update Board.svelte

--- src/Board.svelte
+++ src/Board.svelte
@@ -1,52 +1,94 @@
 <script>
-  let count = 0;
+  import { onMount, afterUpdate } from "svelte";
 
-  function handleClick() {
-    count += 1;
-  }
+  const initialBoard = Array.from(new Array(9));
 
-  let board = [
-    [false, false, false],
-    [false, false, false],
-    [false, false, false]
-  ];
+  let board = initialBoard;
 
-  let things = [
-    { id: 1, color: "#0d0887" },
-    { id: 2, color: "#6a00a8" },
-    { id: 3, color: "#b12a90" },
-    { id: 4, color: "#e16462" },
-    { id: 5, color: "#fca636" }
-  ];
+  onMount(() => {
+    try {
+      board =
+        JSON.parse(window.localStorage.getItem("tictactoe")) || initialBoard;
+      console.log("state board", board);
+    } catch (e) {}
+  });
+
+  afterUpdate(function() {
+    window.localStorage.setItem("tictactoe", JSON.stringify(board));
+  });
 
   let currentPlayer = "x";
+  let winningPlayer = "";
 
-  function handleFieldClick(i, j) {
-    console.log(i, j);
-    board[i][j] = currentPlayer;
+  function handleFieldClick(i) {
+    board[i] = currentPlayer;
 
+    // switch player
     currentPlayer = currentPlayer === "x" ? "o" : "x";
 
-    checkWinningCondition();
-  }
+    const winningCombination = checkWinningCondition(board);
 
-  function checkWinningCondition() {
-    const reduce = (acc, i) => (acc != String(i) ? acc + i : acc);
-    // const rows = board.map(row => row.reduce(reduce, ""));
-    const rows = board.map(row => row.join(""));
-
-    const size = board[0].length;
-    let columns = ["", "", ""];
-    for (let i = 0; i < size; i++) {
-      for (let j = 0; j < size; j++) {
-        columns[i] = columns[i] + board[j][i];
-      }
+    if (winningCombination) {
+      winningPlayer = getWinningPlayer(winningCombination, board);
     }
-
-    console.log(rows);
-    console.log(columns);
-    // console.log(currentPlayer, "won");
   }
+
+  function getWinningPlayer(winningCombination, board) {
+    return board[winningCombination[0]];
+  }
+
+  function checkWinningCondition(board) {
+    const possibleWinningCombinations = [
+      // rows
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      // columns
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      // diagonals
+      [0, 4, 8],
+      [6, 4, 2]
+    ];
+
+    const get = index => board[index];
+
+    return possibleWinningCombinations
+      .filter(combination => {
+        return (
+          !!get(combination[0]) &&
+          get(combination[0]) === get(combination[1]) &&
+          get(combination[0]) === get(combination[2])
+        );
+      })
+      .pop();
+  }
+
+  function clearState() {
+    // remove the state from local storage
+    localStorage.removeItem("tictactoe");
+
+    // and reset the board
+    board = initialBoard;
+  }
+
+  // create chunks of the board to get rows
+  // const rows = board
+  //   .map((_, i) => i)
+  //   .reduce((acc, x) => {
+  //     if (x % 3 === 0) {
+  //       acc.push([x]);
+  //       return acc;
+  //     } else {
+  //       console.log(typeof acc, acc.length);
+  //       acc[acc.length - 1].push(x);
+  //       return acc;
+  //     }
+  //   }, []);
+
+  // way simpler
+  const rows = [[0, 1, 2], [3, 4, 5], [6, 7, 8]];
 </script>
 
 <style>
@@ -58,27 +100,37 @@
     cursor: pointer;
     width: 25px;
     height: 25px;
+    display: inline-flex;
+    /* text-align: center; */
+    justify-content: center;
+    align-items: center;
     border: 1px solid #aaa;
-    display: inline-block;
-    text-align: center;
+    box-shadow: inset 0 0 10px rgba(200, 200, 200, 0.8);
+    color: #000;
+    font-weight: 600;
   }
 </style>
 
-<button on:click={handleClick}>
-  Clicked {count} {count === 1 ? 'time' : 'times'}
-</button>
 <h1>
-  player
+  next player
   <strong>{currentPlayer}</strong>
 </h1>
-{#each board as row, i}
+
+{#each rows as row}
   <div>
-    {#each row as field, j}
-      <span on:click={() => handleFieldClick(i, j)}>
-        {!!field ? field : '  '}
+    {#each row as index}
+      <span on:click={() => handleFieldClick(index)}>
+        {!!board[index] ? board[index] : '  '}
       </span>
     {/each}
   </div>
 {/each}
 
-{#each things as thing (thing.id)}{thing.color}{/each}
+{#if winningPlayer}
+  <h1>
+    winner
+    <strong>{winningPlayer}</strong>
+  </h1>
+{/if}
+
+<button on:click={clearState}>start over</button>
